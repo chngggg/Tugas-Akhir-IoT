426b4f53ac55f66f6d75a517b1f1df02325b2b94
chngggg <133532081+chngggg@users.noreply.github.com>
2023-12-14 19:06:09 +0700

    Add files via upload

---
+++ monitor_aceng/getdata.php
@@ -0,0 +1,22 @@
+<?php
+// konfigurasi database
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$db = "iot";
+$koneksi = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
+
+    $data = mysqli_query($koneksi, "SELECT * from suhu ORDER BY jam DESC LIMIT 1"); // ambil data terbaru saja
+    $no=1;
+    foreach ($data as $row){
+	$datax=[	    
+        	"jam"=>@$row['jam'],
+		"suhu"=>(float)@$row['suhu']
+   		];
+    }
+
+    // $datax=["jam"=>date("Y-m-d H:i:s"), "suhu"=>12];
+    echo json_encode($datax);
+
+    ?>
+
